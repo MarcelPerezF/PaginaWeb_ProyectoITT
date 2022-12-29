de08c23b2be5252512aec99a9954060328ac3589
MarcelPerezF <94399558+MarcelPerezF@users.noreply.github.com>
2022-12-29 12:53:27 -0300

    Version 3 Pagina Web

---
+++ Js/Js.js
@@ -0,0 +1,8 @@
+//document.write(5 + 6);
+// $(function() {
+//     // console.log( "ready!" );
+//     var fecha = new Date();
+// //alert("Día: "+fecha.getDate()+"\nMes: "+(fecha.getMonth()+1)+"\nAño: "+fecha.getFullYear());
+// //alert("Hora: "+fecha.getHours()+"\nMinuto: "+fecha.getMinutes()+"\nSegundo: "+fecha.getSeconds()+"\nMilisegundo: "+fecha.getMilliseconds());
+// });
+
