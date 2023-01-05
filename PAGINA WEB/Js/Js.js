3cc3201c3ba14e8b1f37cc2eca4c45d2ce049752
MarcelPerezF <94399558+MarcelPerezF@users.noreply.github.com>
2023-01-05 13:22:55 -0300

    Delete PAGINA WEB directory

--- PAGINA WEB/Js/Js.js
+++
@@ -1,8 +0,0 @@
-//document.write(5 + 6);
-// $(function() {
-//     // console.log( "ready!" );
-//     var fecha = new Date();
-// //alert("Día: "+fecha.getDate()+"\nMes: "+(fecha.getMonth()+1)+"\nAño: "+fecha.getFullYear());
-// //alert("Hora: "+fecha.getHours()+"\nMinuto: "+fecha.getMinutes()+"\nSegundo: "+fecha.getSeconds()+"\nMilisegundo: "+fecha.getMilliseconds());
-// });
-
